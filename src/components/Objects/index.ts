6a8fe36429d268c8a33efc737173e8c6935f78fc
HuangYu <1090165925@qq.com>
2020-12-07 09:38:07 +0800

    change npc numbers settings

--- src/components/Objects/index.ts
+++ src/components/Objects/index.ts
@@ -12,6 +12,7 @@ import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 const { x, z, width, height } = mapParams;
 
 export class System {
+  private npcNum = 200;
   private _npcs: Array<NPC> = [];
   private _player: Player | null = null;
   private map = new Array(rowNum).fill(0).map(() => new Array(colNum).fill(1));
@@ -22,7 +23,7 @@ export class System {
   private shadowPath = 'textures/roundshadow.png';
   constructor(scene: Scene) {
     const gene = () => Math.random() * 40 - 20;
-    this.npcData = new Array(200).fill(0).map(() => ({ x: gene(), z: gene() }));
+    this.npcData = new Array(this.npcNum).fill(0).map(() => ({ x: gene(), z: gene() }));
     this.initTerrain(scene);
     const staticObjs = data.map(({ pos, size, rotation }) => new Wall(pos, size, rotation, scene));
     this.quadTree = new QuadTree(new Rect(x - 5, z - 5, width + 10, height + 10));
